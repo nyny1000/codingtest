ab63b66d1a8f8fc2bf848ac761bc6512b6b6371d
nyny1000 <rnjsskdud941@naver.com>
2021-01-09 23:12:00 +0900

    hanoi 1

--- 1st_week/src/Hanoi.java
+++ 1st_week/src/Hanoi.java
@@ -10,27 +10,30 @@ public class Hanoi {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-
+ 
 		hanoi(n, 1, 2, 3);
 		System.out.println(move);
 		System.out.print(sb);
 
 	}
 
-	public static void hanoi(int n, int start, int mid, int to) {
+//	start: 원판이 있던 위치
+//	to: 원판을 옮기고자 하는 위치
+//	mid: 원판을 옮길 때 거쳐가는 위치
+	public static void hanoi(int n, int from, int mid, int to) {
 
 		move++;
 
-		// basecase: 원판이 하나 남았을 때
+		// base case: 원판이 하나 남았을 때
 		if (n == 1) {
-			sb.append(start + " " + to + "\n");
+			sb.append(from + " " + to + "\n");
 			return;
 		}
 
 		else {
-			hanoi(n - 1, start, to, mid); // 1번째 탑에 있던 n-1개의 원판을 2번째 탑으로 이동
-			sb.append(start + " " + to + "\n"); // 1번째 탑에 있던 가장 큰 원판을 3번째 탑으로 이동
-			hanoi(n - 1, mid, start, to); // 2번째 탑에 있던 모든 원판을 3번째 탑으로 이동
+			hanoi(n - 1, from, to, mid); // 1번째 탑에 있던 n-1개의 원판을 2번째 탑으로 이동
+			sb.append(from + " " + to + "\n"); // 1번째 탑에 있던 가장 큰 원판을 3번째 탑으로 이동
+			hanoi(n - 1, mid, from, to); // 2번째 탑에 있던 모든 원판을 3번째 탑으로 이동
 		}
 
 	}
